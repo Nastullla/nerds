8294f425b402a227000e9fd69164c1fd530ef8fd
Nastullla <30768051+Nastullla@users.noreply.github.com>
2017-10-10 22:43:10 +0300

    Меню

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,10 @@
 var btnWrite = document.querySelector(".write-to-us");
 var btnClose = document.querySelector(".close");
+var btnSubmit = document.querySelector(".feedback");
 var popup = document.querySelector(".modal");
-
-// var form = popup.querySelector("form");
-
-// var arrivalDate = popup.querySelector("[name=arrival-date]");
-// var departureDate = popup.querySelector("[name=departure-date]");
-// var grownups = popup.querySelector("[name=grownups]");
-// var children = popup.querySelector("[name=children]");
-
-// var storageGrownups = localStorage ? localStorage.getItem("grownups") : null;
-// var storageChildren = localStorage ? localStorage.getItem("children") : null;
-
-// if (storageGrownups) {
-      // grownups.value = storageGrownups;
-// } 
-
-// if (storageChildren) {
-      // children.value = storageChildren;
-// } 
+var nameValue = popup.querySelector("[name=name]");
+var mailValue = popup.querySelector("[name=mail]");
+var letterValue = popup.querySelector("[name=letter]");
 
 btnWrite.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -30,21 +16,15 @@ btnClose.addEventListener("click", function (evt) {
   popup.classList.add("modal-hide");
 });
 
+function ValidateField(field, evt) {
+  if (!field.value) {
+    field.classList.add("validaition-error");
+    evt.preventDefault();
+  }  
+}
 
-
-
-/*form.addEventListener("submit", function (evt) {
-    if (!arrivalDate.value || !departureDate.value 
-    	|| Number(grownups.value) < 1 || Number(children.value) < 0) {
-      	evt.preventDefault();
-      	popup.classList.remove("modal-show");
-      	popup.classList.remove("modal-error");
-      	popup.offsetWidth = popup.offsetWidth;
-      	popup.classList.add("modal-error");
-    } else {
-    	if (localStorage) {
-      		localStorage.setItem("grownups", grownups.value);
-      		localStorage.setItem("children", children.value);
-      	}
-    }
-});*/
+btnSubmit.addEventListener("click", function (evt) {
+  ValidateField(nameValue, evt);
+  ValidateField(mailValue, evt);
+  ValidateField(letterValue, evt);
+});
